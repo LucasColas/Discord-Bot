6358ca990d9121863dff6b1cdb1ec9e738f9d9d5
Lucas Colas <51634013+LucasColas@users.noreply.github.com>
2020-05-31 17:02:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,21 +7,22 @@ client = discord.Client()
 
 @client.event
 async def on_message(message):
+    spec_channel = client.get_channel(427899196985704450)
     id = client.get_guild(325925436804825111)
+    #id_autobots = 427899196985704450
+
     if message.author == client.user:
         return
 
-    #ch = client.get_channel(427899196985704450)
-    #if message.author.id == 427899196985704450:
-    if message.content.find("!Test") != -1 :
-      await message.channel.send("Operational !")
+    if message.channel == spec_channel:
+      if message.content == "!Test":
+        await spec_channel.send("Yes")
       
-    if message.content.find("!Hello") != -1 :
-      await message.channel.send("Hi")
+      if message.content.find("!Hello") != -1 :
+        await spec_channel.send("Hi")
 
-    if message.content == "!users": #Number of users
-      await message.channel.send(f"users : {id.member_count}.")
-
+      if message.content == "!users": #Number of users
+        await spec_channel.send(f"users : {id.member_count}.")
 
     
     if message.content.find("Salut") != -1 :
